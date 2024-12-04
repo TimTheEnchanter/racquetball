5b8d0e0080a6330f16739b182873794a4e49f85b
James Parrott <japarrott@shaw.ca>
2024-12-04 14:47:09 -0500

    instantiate the ball and add option for 3rd and 4th players

--- main.ts
+++ main.ts
@@ -137,3 +137,34 @@ if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Two))) {
     mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setStayInScreen(true)
     mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).y = 110
 }
+if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Three))) {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three), sprites.create(assets.image`paddle3`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Three), 100, 0)
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)).setStayInScreen(true)
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Three)).y = 110
+}
+if (mp.isConnected(mp.playerSelector(mp.PlayerNumber.Four))) {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four), sprites.create(assets.image`paddle4`, SpriteKind.Player))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Four), 100, 0)
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)).setStayInScreen(true)
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Four)).y = 110
+}
+let ball = sprites.create(img`
+    . . . . . b b b b b b . . . . . 
+    . . . b b 9 9 9 9 9 9 b b . . . 
+    . . b b 9 9 9 9 9 9 9 9 b b . . 
+    . b b 9 d 9 9 9 9 9 9 9 9 b b . 
+    . b 9 d 9 9 9 9 9 1 1 1 9 9 b . 
+    b 9 d d 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 d 9 9 9 9 9 9 1 1 1 9 9 9 b 
+    b 9 3 9 9 9 9 9 9 9 9 9 1 9 9 b 
+    b 5 3 d 9 9 9 9 9 9 9 9 9 9 9 b 
+    b 5 3 3 9 9 9 9 9 9 9 9 9 d 9 b 
+    b 5 d 3 3 9 9 9 9 9 9 9 d d 9 b 
+    . b 5 3 3 3 d 9 9 9 9 d d 5 b . 
+    . b d 5 3 3 3 3 3 3 3 d 5 b b . 
+    . . b d 5 d 3 3 3 3 5 5 b b . . 
+    . . . b b 5 5 5 5 5 5 b b . . . 
+    . . . . . b b b b b b . . . . . 
+    `, SpriteKind.Projectile)
+ball.setBounceOnWall(true)
